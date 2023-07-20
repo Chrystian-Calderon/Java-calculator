51de1bfd2be75e3bc86dade42bd5c672a9d8b22c
Chrystian <chrystianralfc@gmail.com>
2023-07-20 18:26:38 -0400

    Calculadora que puede resolver operaciones basicas pero no del todo los numeros negativos

--- src/Interface/Calculator.java
+++ src/Interface/Calculator.java
@@ -7,6 +7,9 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JTextField;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
@@ -45,20 +48,23 @@ public class Calculator extends JFrame {
 	}
 
 	/*
-	 * concatenar
+	 * concatenate
 	 */
-	private static boolean searchPoint(String getText, String[] operations) {
+	private static boolean decimalPoint(String getText, String[] operations) {
+		//invert text
+		StringBuilder auxText = new StringBuilder(getText);
+		String newText = auxText.reverse().toString();
+		
 		boolean found = false;
-		for(int index = getText.length() - 1; index >= 0; index--) {
-			String character = String.valueOf(getText.charAt(index));
-			if(Arrays.asList(operations).contains(character)) {
-				if(character == ".") {
-					found = true;
-					break;
-				}
+		for(char character : newText.toCharArray()) {
+			if( Arrays.asList(operations).contains(String.valueOf(character)) ) {
+				break;
+			} else if( character == '.' ) {
+				found = true;
+				break;
 			}
 		}
-		System.out.println(getText.charAt(0));
+
 		return found;
 	}
 	
@@ -67,34 +73,168 @@ public class Calculator extends JFrame {
 		String newText = "";
 		String[] operations = {"/", "x", "+", "-", "."};
 		boolean contains = false;
-		if(Arrays.asList(operations).contains(text)) {
+		
+		if( text == "." && textResult.length() > 0 ) {
 			String lastElement = textResult.substring(textResult.length() - 1);
-			//try
-			for(String operation : operations) {
-				if(lastElement.equals(operation)) {
-					contains = true;
-					break;
+			if( !(Arrays.asList(operations).contains(lastElement)) ) {
+				if( !decimalPoint(textResult, Arrays.copyOfRange(operations, 0, 4)) ) {
+					result.setText(textResult + text);
+				}
+			} else {
+				newText = removeOne(result);
+				result.setText(newText);
+				textResult = result.getText();
+				System.out.println(textResult);
+				if( !decimalPoint(textResult, Arrays.copyOfRange(operations, 0, 4)) ) {
+					result.setText(textResult + text);
 				}
 			}
-
+		} else if( Arrays.asList(operations).contains(text) && textResult.length() > 0 ) {
+			String lastElement = textResult.substring(textResult.length() - 1);
+			//try 
+			for(String operation : operations) {
+				if(lastElement.equals(operation)) {
+					contains = true; break;
+				}
+			}
+			
 			if(contains) {
 				newText = removeOne(result);
 				result.setText(newText + text);
 			} else {
 				result.setText(textResult + text);
 			}
-			System.out.println(searchPoint(textResult, operations));
 		} else {
 			result.setText(textResult + text);
 		}
 	}
 	
-	
 	/*
-	 * Eliminar
+	 * math operation
 	 */
-	public static void eliminate() {
+	private static boolean searchOperators(String[] operations, String text) {
+		boolean containsOperator = false;
+		for(String operation : operations) {
+			if( text.contains(operation) ) {
+				containsOperator = true;
+				break;
+			}
+		}
+		return containsOperator;
+	}
+	
+	private static int[] searchNumbers(int index, String text, String[] operations) {
+		int posMax = index;
+		int posMin = index;
+		for( int varAux = index + 1; varAux < text.length(); varAux++ ) {
+			String character = String.valueOf(text.charAt(varAux));
+			if( !(Arrays.asList(operations).contains(character)) ) {
+				posMax++;
+			} else {
+				if( text.charAt(index + 1) == '-' ) {
+					posMax++;
+				} else {
+					break;
+				}
+			}
+		}
+		for( int varAux = index - 1; varAux > -1; varAux-- ) {
+			String character = String.valueOf(text.charAt(varAux));
+			if( !(Arrays.asList(operations).contains(character)) ) {
+				posMin--;
+			} else {
+				break;
+			}
+		}
 		
+		int[] position = {posMax, posMin};
+		return position;
+	}
+	
+	private static String solve(int indexStart, int indexEnd, int index, String operation, String text) {
+		System.out.println(indexStart + " " + indexEnd + " " + index);
+		double num1 = Double.parseDouble(text.substring(indexStart, index));
+		double num2 = Double.parseDouble(text.substring(index + 1, indexEnd + 1));
+		double result = 0;
+		String exit = "";
+		
+		switch(operation) {
+		case "+":
+			result = num1 + num2;
+			break;
+		case "-":
+			result = num1 - num2;
+			break;
+		case "x":
+			result = num1 * num2;
+			break;
+		case "/":
+			if(num2 != 0) {
+				result = num1 / num2;
+			}
+			break;
+		default:
+			System.out.println("No existe operador");
+			break;
+		}
+		
+		exit = Double.toString(result);
+		return exit;
+	}
+	
+	public static void getResult(JTextField result) {
+		String text = result.getText();
+		String[] operations = {"/", "x", "+", "-"};
+		int size = text.length();
+		int posMax = 0, posMin = 0;
+		
+		boolean operators = true;
+		
+		while(operators) {
+			int index = 0;
+			while( index < size ) {
+				if( text.charAt(0) == '-' ) {
+					operators = false;
+					break;
+				}
+				if( searchOperators(Arrays.copyOfRange(operations, 0, 2), text) ) {
+					System.out.println("Entra");
+					if( text.charAt(index) == '/' || text.charAt(index) == 'x' ) {
+						int[] posicion = searchNumbers(index, text, operations);
+						posMin = posicion[1];
+						posMax = posicion[0];
+						String character = String.valueOf(text.charAt(index));
+						String operationResult = solve(posMin, posMax, index, character, text);
+						System.out.println(operationResult);
+						text = text.replace(text.substring(posMin, posMax + 1), operationResult);
+						index = size;
+					} else {
+						index++;
+					}
+					
+				} else {
+					System.out.println("opcion2");
+					if( searchOperators(Arrays.copyOfRange(operations, 2, 4), text) ) {
+						if( text.charAt(index) == '-' || text.charAt(index) == '+' ) {
+							int[] posicion = searchNumbers(index, text, operations);
+							posMin = posicion[1];
+							posMax = posicion[0];
+							String character = String.valueOf(text.charAt(index));
+							String operationResult = solve(posMin, posMax, index, character, text);
+							text = text.replace(text.substring(posMin, posMax + 1), operationResult);
+							index = size;
+						} else {
+							index++;
+						}
+					} else {
+						operators = false;
+						index = size;
+					}
+				}
+			}
+			System.out.println(text);
+		}
+		result.setText(text);
 	}
 	
 	/**
@@ -143,6 +283,11 @@ public class Calculator extends JFrame {
 		background.add(btnPoint);
 		
 		JButton btnEqual = new JButton("=");
+		btnEqual.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				getResult(result);
+			}
+		});
 		btnEqual.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		btnEqual.setBounds(150, 356, 60, 60);
 		background.add(btnEqual);
@@ -291,11 +436,16 @@ public class Calculator extends JFrame {
 		background.add(btnSubtract);
 		
 		JButton btnDelete = new JButton("C");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				result.setText("");
+			}
+		});
 		btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		btnDelete.setBounds(220, 72, 60, 60);
 		background.add(btnDelete);
 		
-		JButton btnDeleteOne = new JButton("<-");
+		JButton btnDeleteOne = new JButton("<");
 		btnDeleteOne.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String newText = removeOne(result);
